eae2c2cde0191b946d21dd60f3d01b98da7b6a67
ZachMcGuire <zach.mcguire09@gmail.com>
2024-03-20 05:46:20 -0900

    test

---
+++ tpq-site/src/App.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from "./SignUpForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
--- tpq-site/src/firebase-config.jsx
+++ tpq-site/src/firebase-config.jsx
@@ -1,23 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_wSc8B9SpQEHub-aMnuDPxdtFVsoz_NU",
-  authDomain: "tpq-server-51cd3.firebaseapp.com",
-  projectId: "tpq-server-51cd3",
-  storageBucket: "tpq-server-51cd3.appspot.com",
-  messagingSenderId: "356831174005",
-  appId: "1:356831174005:web:fdcafb234f2e705c60473c",
-  measurementId: "G-CCYFHT400E"
+  apiKey: "AIzaSyAB0CUE8ZFbpB49aHsk5ns0cQoJmtZa0Zc",
+  authDomain: "tpq-site.firebaseapp.com",
+  projectId: "tpq-site",
+  storageBucket: "tpq-site.appspot.com",
+  messagingSenderId: "437060106178",
+  appId: "1:437060106178:web:f55e3fe9f2f640ff6a085b",
+  measurementId: "G-DKKWN5VVJ8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
+
+export { db };
